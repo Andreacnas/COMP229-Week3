b50a212e1dc562517671be620cf376db2dd19a6d
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-09-23 19:10:35 -0400

    Added Express and third-party modules

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-//Import third-party nodejs module Connect
-const connect = require('connect');
+//Third party Modules
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan';
+import session from "express-session";
+
+// ES Modules fix for __dirname
+import path, {dirname} from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Instatiate app-server
-const app = connect();
+const app = express();
 
 //custom middleware
 function helloWorld(req,res,next){
@@ -17,11 +25,12 @@ function byeWorld(req,res,next){
 }
 
 //app middleware to connect application
+app.use(helloWorld);
 app.use('/hello',helloWorld);
 app.use('/bye',byeWorld);
 
 //Run app
-app.listen(3001);
+app.listen(3000);
 
-console.log('Server running at http://locahost:3001');
+console.log('Server running at http://localhost:3000');
 
