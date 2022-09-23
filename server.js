d0e7b41371294525bd7e303146e52b990157c42f
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-09-23 19:20:05 -0400

    Completed third party Express Middleware activation

--- server.js
+++ server.js
@@ -4,13 +4,28 @@ import cookieParser from "cookie-parser";
 import logger from 'morgan';
 import session from "express-session";
 
-// ES Modules fix for __dirname
+//ES Modules fix for __dirname
 import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Instatiate app-server
 const app = express();
+
+//Setup ViewEngine EJS
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__firname, '../public')));
+app.use(session({
+    secret: 'MySecret',
+    saveUnitialized: false,
+    resave: false
+}));
 
 //custom middleware
 function helloWorld(req,res,next){
