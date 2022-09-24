ed6f0a6be1921d91f03eae619a1b5cdf07173bb2
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-09-23 20:43:30 -0400

    views and partials completed

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,4 +1,20 @@
 //Index Controller
-export function helloWorld(req,res,next){
-    res.render('index', { title: 'Home' });
+export function displayHomePage(req,res,next){
+    res.render('index', { title: 'Home', page: 'home' });
+};
+
+export function displayAboutPage(req,res,next){
+    res.render('index', { title: 'About', page: 'about' });
+};
+
+export function displayProjectPage(req,res,next){
+    res.render('index', { title: 'Project', page: 'project' });
+};
+
+export function displayServicesPage(req,res,next){
+    res.render('index', { title: 'Services', page: 'services' });
+};
+
+export function displayContactPage(req,res,next){
+    res.render('index', { title: 'Contactt', page: 'contact' });
 };
--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,12 +1,20 @@
 //Index Routes
 import { Router } from 'express';
-import { helloWorld } from '../controllers/index.controller.server.js'
+import { displayHomePage, 
+         displayAboutPage,
+         displayProjectPage,
+         displayServicesPage,
+         displayContactPage } from '../controllers/index.controller.server.js'
 
 //Instatiate the router
 const router = Router();
 
 //add middleware to connect application
-router.get('/',helloWorld);
-router.get('/hello',helloWorld);
+router.get('/',displayHomePage);
+router.get('/home',displayHomePage);
+router.get('/about',displayAboutPage);
+router.get('/project',displayProjectPage);
+router.get('/services',displayServicesPage);
+router.get('/contact',displayContactPage);
 
 export default router;
