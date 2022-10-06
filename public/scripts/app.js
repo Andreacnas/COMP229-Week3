731c3316e2c6e317c7f46df5916b3f18c87fbacc
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-10-06 18:45:25 -0400

    created about css file and updated js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,8 @@
+// JS file
+// Name: Andrea Cavalcanti Nascimento
+// Student Number: 301250129
+// Date: Oct 7th, 2022
+
 //IIFE
 (function(){
     function Start(){
@@ -6,3 +11,48 @@
 
     window.addEventListener('load', Start);
 })();
+
+//***** HOME PAGE *****
+//Redirect to about page
+function clickAbout(){
+    window.location.href="./about"
+}
+
+//***** ABOUT PAGE *****
+//Redirect to contact page
+function clickContact(){
+    window.location.href="./contact"
+}
+
+//***** CONTACT PAGE *****
+//Carroussel
+var i = 0; 
+var images = [];
+var time = 1000;
+
+//Image List
+images[0] = './content/images/Avatar1.png';
+images[1] = './content/images/Avatar2.png';
+images[2] = './content/images/Avatar3.png';
+
+
+//Slideshow
+function slideImg(){
+    document.slide.src = images[i];
+
+    if (i< images.length - 1) {
+        i++;
+    } else {
+        i =0;
+    }
+
+    setTimeout ("slideImg()", time);
+
+}
+
+window.onload = slideImg;
+
+//Redirect to Home page
+function clickSend(){
+    window.location.href="./home"
+}
--- public/scripts/home.js
+++
@@ -1,9 +0,0 @@
-// Home JS file
-// Name: Andrea Cavalcanti Nascimento
-// Student Number: 301250129
-// Date: Oct 7th, 2022
-
-//Redirect to About page
-function clickAbout(){
-    window.location.href="./about"
-}
